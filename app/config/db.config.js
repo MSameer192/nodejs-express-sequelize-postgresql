873b3fbffa8565528ef7e44e0015a9a4f289f42d
MSameer192 <sameer192.official@gmail.com>
2023-04-15 02:38:23 +0500

    one to many

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,14 +1,13 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "sameer",
-    DB: "practicesequelize",
-    dialect: "postgres",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
-  
+  HOST: "localhost",
+  USER: "postgres",
+  PASSWORD: "sameer",
+  DB: "onetomany",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
--- app/controllers/tutorial.controller.js
+++ app/controllers/tutorial.controller.js
@@ -1,154 +1,72 @@
 const db = require("../models");
 const Tutorial = db.tutorials;
-const Op = db.Sequelize.Op;
+const Comment = db.comments;
 
-// Create and Save a new Tutorial
-exports.create = (req, res) => {
-    // Validate request
-    if (!req.body.title) {
-        res.status(400).send({
-            message: "Content can not be empty!"
-        });
-        return;
-    }
+//  Create and Save new Tutorials
 
-    // Create a Tutorial
-    const tutorial = {
-        title: req.body.title,
-        description: req.body.description,
-        published: req.body.published ? req.body.published : false
-    };
-
-    // Save Tutorial in the database
-    Tutorial.create(tutorial)
-        .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while creating the Tutorial."
-            });
-        });
-};
-
-
-// Retrieve all Tutorials from the database.
-exports.findAll = (req, res) => {
-    const title = req.query.title;
-    var condition = title ? { title: { [Op.iLike]: `%${title}%` } } : null;
-
-    Tutorial.findAll({ where: condition })
-        .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while retrieving tutorials."
-            });
-        });
-};
-
-
-// Find a single Tutorial with an id
-exports.findOne = (req, res) => {
-    const id = req.params.id;
-
-    Tutorial.findByPk(id)
-        .then(data => {
-            if (data) {
-                res.send(data);
-            } else {
-                res.status(404).send({
-                    message: `Cannot find Tutorial with id=${id}.`
-                });
-            }
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: "Error retrieving Tutorial with id=" + id
-            });
-        });
-};
-
-// Update a Tutorial by the id in the request
-exports.update = (req, res) => {
-    const id = req.params.id;
-
-    Tutorial.update(req.body, {
-        where: { id: id }
+exports.createTutorial = (tutorial) => {
+    return Tutorial.create({
+        title: tutorial.title,
+        description: tutorial.description,
     })
-        .then(num => {
-            if (num == 1) {
-                res.send({
-                    message: "Tutorial was updated successfully."
-                });
-            } else {
-                res.send({
-                    message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`
-                });
-            }
+        .then((tutorial) => {
+            console.log(">> Created tutorial: " + JSON.stringify(tutorial, null, 4));
+            return tutorial;
         })
-        .catch(err => {
-            res.status(500).send({
-                message: "Error updating Tutorial with id=" + id
-            });
+        .catch((err) => {
+            console.log(">> Error while creating tutorial: ", err);
         });
 };
 
-// Delete a Tutorial with the specified id in the request
-exports.delete = (req, res) => {
-    const id = req.params.id;
+//  Create and Save new Comments
 
-    Tutorial.destroy({
-        where: { id: id }
+exports.createComment = (tutorialId, comment) => {
+    return Comment.create({
+        name: comment.name,
+        text: comment.text,
+        tutorialId: tutorialId,
     })
-        .then(num => {
-            if (num == 1) {
-                res.send({
-                    message: "Tutorial was deleted successfully!"
-                });
-            } else {
-                res.send({
-                    message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
-                });
-            }
+        .then((comment) => {
+            console.log(">> Created comment: " + JSON.stringify(comment, null, 4));
+            return comment;
         })
-        .catch(err => {
-            res.status(500).send({
-                message: "Could not delete Tutorial with id=" + id
-            });
+        .catch((err) => {
+            console.log(">> Error while creating comment: ", err);
         });
 };
 
-// Delete all Tutorials from the database.
-exports.deleteAll = (req, res) => {
-    Tutorial.destroy({
-        where: {},
-        truncate: false
-    })
-        .then(nums => {
-            res.send({ message: `${nums} Tutorials were deleted successfully!` });
+// Get the comments for a given tutorial
+
+exports.findTutorialById = (tutorialId) => {
+    return Tutorial.findByPk(tutorialId, { include: ["comments"] })
+        .then((tutorial) => {
+            return tutorial;
         })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while removing all tutorials."
-            });
+        .catch((err) => {
+            console.log(">> Error while finding tutorial: ", err);
         });
 };
 
-// Find all published Tutorials
-exports.findAllPublished = (req, res) => {
-    Tutorial.findAll({ where: { published: true } })
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while retrieving tutorials."
+
+// Get the comments for a given comment id
+
+exports.findCommentById = (id) => {
+    return Comment.findByPk(id, { include: ["tutorial"] })
+        .then((comment) => {
+            return comment;
+        })
+        .catch((err) => {
+            console.log(">> Error while finding comment: ", err);
         });
-      });
-  };
+};
+
+
+// Get all Tutorials include comments
+
+exports.findAll = () => {
+    return Tutorial.findAll({
+        include: ["comments"],
+    }).then((tutorials) => {
+        return tutorials;
+    });
+};
